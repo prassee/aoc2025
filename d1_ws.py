8b364511af2dbd6c489e1bad25b8a30248d788f2
Prasanna <Prasanna.kumar@lendingkart.com>
2025-12-02 13:55:06 +0530

    d1

--- d1_ws.py
+++ d1_ws.py
@@ -1,15 +1,26 @@
 # %%
-dial = list(range(0, 100))
-current_position = 50
 
+class DialerAnalyzer:
 
-def update_dial_pos(new_pos: int):
-    global current_position
-    projected = current_position + new_pos
-    if projected >= len(dial):
-        projected = projected % len(dial)
-    current_position = dial[projected]
-    print(f"current position {current_position}")
+    def __init__(self, dial_size: int = 100, start_pos: int = 50) -> None:
+        self.dial = list(range(dial_size))
+        self.current_position = start_pos
+
+    def update_dial_pos(self, new_pos: int):
+        projected = self.current_position + new_pos
+        if projected >= len(self.dial):
+            projected = projected % len(self.dial)
+        self.current_position = self.dial[projected]
+        return self.current_position
+
+    def analyze_dial_sequence(self, input_seq):
+        zeros = []
+        for x in input_seq:
+            val = int(x.replace("L", "-").replace("R", ""))
+            pos = self.update_dial_pos(val)
+            if pos == 0:
+                zeros.append(x)
+        return zeros, len(zeros)
 
 
 # %%
@@ -25,6 +36,5 @@ input_seq = [
     "R14",
     "L82",
 ]
-for x in input_seq:
-    val = int(x.replace("L", "-").replace("R", ""))
-    update_dial_pos(val)
+da = DialerAnalyzer()
+da.analyze_dial_sequence(input_seq)
